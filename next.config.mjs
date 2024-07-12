ec31be6dcb232d59d4206ff51c9994509fe13e6b
AlbertAbb <albertoabb.dev@gmail.com>
2024-07-12 10:57:46 +0200

    Removed typescript and eslint

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import {withSentryConfig} from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {ignoreBuildErrors: true},
+    eslint: {ignoreDuringBuilds: true}
 };
 
 export default withSentryConfig(nextConfig, {
