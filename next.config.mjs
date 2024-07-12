7602011f761406169b8e4a70e3bb87ba955ebc0b
AlbertAbb <albertoabb.dev@gmail.com>
2024-07-12 10:59:01 +0200

    Commented typescript and eslint

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@ import {withSentryConfig} from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {ignoreBuildErrors: true},
-    eslint: {ignoreDuringBuilds: true}
+    // typescript: {ignoreBuildErrors: true},
+    // eslint: {ignoreDuringBuilds: true}
 };
 
 export default withSentryConfig(nextConfig, {
